ea129e4a6c2a04812335cb27ebd09ae0e0c93852
Andrey Reznik <andrey.reznik@yahoo.com>
2019-10-16 11:11:09 +0300

    Remove debug log

--- gulp/tasks/args.js
+++ gulp/tasks/args.js
@@ -3,7 +3,6 @@ module.exports = function () {
     $.gulp.task('args', function () {
         return new Promise(function (resolve, reject) {
             $.gp.yargs.argv.release ? $.config.release = true : null
-            console.log($.config.release);
             resolve();
         });
     });
